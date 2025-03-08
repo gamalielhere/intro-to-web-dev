5f179d858444c316a4321d3f0c88ea1ca1082ffd
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-03-08 13:31:06 -0800

    feat: update things on footers

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,6 @@
 function formSubmit(e) {
   const form = document.getElementById("emailForm");
   e.preventDefault();
-  e.stopPropagation();
   alert("Thank you for subscribing!");
   form.reset();
 }
