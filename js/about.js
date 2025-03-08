4fc509f79f8b2acfada003a2f4c12751a461b9e8
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-03-08 14:57:59 -0800

    feat: use local storage for about page

--- js/about.js
+++ js/about.js
@@ -1,7 +1,10 @@
 function contactUsSubmit(e) {
+  const formValues = new FormData(e.target);
+  const obj = Object.fromEntries(formValues);
   const form = document.getElementById("contactUsForm");
   e.preventDefault();
   e.stopPropagation();
   alert("Thank you for contacting us!");
+  window.localStorage.setItem(`${obj.fname} ${obj.lname}`, JSON.stringify(obj));
   form.reset();
 }
